fe7039d8d80a258edf054690791ad73f647a3d54
Bretton Raymond <bretton.raymond@gmail.com>
2015-12-22 10:06:17 -0500

    Develop view model in pro.js

--- app/scripts/pro.js
+++ app/scripts/pro.js
@@ -31,7 +31,7 @@ var calculator = {
   enterInput: function(){
     calculator.updateDataModel(this.dataset.math, this.dataset.value);
     calculator.updateViewModel(this.dataset.math, this.dataset.value);
-    calculator.render();
+    calculator.render(this.dataset.math, this.dataset.value);
   },
   updateDataModel: function(type, value){
     if(type === 'value'){
@@ -72,6 +72,14 @@ var calculator = {
     if(type === 'clear' && value === 'AC') {
       calculator.dataModel.dataArray = [];
       calculator.dataModel.result = '';
+      calculator.dataModel.dataObj = {};
+    }
+    if(type === 'sign'){
+      if (calculator.dataModel.buffer.charAt(0) === '-') {
+        calculator.dataModel.buffer = calculator.dataModel.buffer.substr(1);
+      }else{
+        calculator.dataModel.buffer = '-' + calculator.dataModel.buffer;
+      }
     }
 
   },
@@ -97,14 +105,100 @@ var calculator = {
     });
     return answer.value;
   },
-  updateViewModel: function(){
-    if(calculator.dataModel.dataArray === [] && calculator.dataModel.buffer === ''){
+  updateViewModel: function(type, value){
+    if(calculator.dataModel.dataArray.length === 0 && calculator.dataModel.buffer === ''){
 
     }
+
+    if (calculator.dataModel.dataArray.length > 0) {
+      switch (calculator.dataModel.dataObj.operator) {
+        case 'add':
+          calculator.dataModel.symbol = '+';
+          break;
+        case 'subtract':
+          calculator.dataModel.symbol = '-';
+          break;
+        case 'multiply':
+          calculator.dataModel.symbol = '*';
+          break;
+        case 'divide':
+          calculator.dataModel.symbol = '/';
+          break;
+        default:
+          console.log('test');
+      }
+    }
+
+    if(type === 'compute'){
+
+
+
+      switch (calculator.dataModel.dataArray[calculator.dataModel.dataArray.length - 1].operator) {
+        case 'add':
+          calculator.dataModel.symbol = '+';
+          break;
+        case 'subtract':
+          calculator.dataModel.symbol = '-';
+          break;
+        case 'multiply':
+          calculator.dataModel.symbol = '*';
+          break;
+        case 'divide':
+          calculator.dataModel.symbol = '/';
+          break;
+        default:
+          console.log('default case view switch');
+      }
+
+
+    }
+    if(calculator.dataModel.dataArray.length > 1){
+      var array = calculator.dataModel.dataArray.slice(0, calculator.dataModel.dataArray.length - 1);
+      calculator.viewModel.leftSide = calculator.parseArray(array);
+    }
+
+    console.log('left side: ' + calculator.viewModel.leftSide);
+
+    calculator.viewModel.rightSide = calculator.dataModel.buffer;
+
+
+    if (type === 'compute') {
+      calculator.viewModel.operator = calculator.dataModel.dataArray[calculator.dataModel.dataArray.length - 1].operator;
+
+    }
+
+    calculator.viewModel.operator = calculator.dataModel.dataObj.operator;
+
     calculator.viewModel.result =  calculator.dataModel.result;
   },
-  render: function(){
-    calculator.$input[0].innerHTML = calculator.viewModel.result;
+  render: function(type, value){
+
+    var output = '';
+
+
+    output = calculator.viewModel.leftSide + ' ' + calculator.viewModel.operator + ' ' + calculator.viewModel.rightSide;
+    /*
+    if (calculator.viewModel.leftSide !== '') {
+      output += calculator.viewModel.leftSide;
+      output += ' ' + calculator.dataModel.symbol + ' ';
+    }
+    if (calculator.dataModel.buffer !== '' && calculator.dataModel.dataArray.length === 0) {
+      output += calculator.viewModel.rightSide;
+    }
+    if (type === 'operator') {
+      output = calculator.viewModel.leftSide + ' ' + calc
+    }
+    */
+    if (type === 'compute') {
+      output += calculator.viewModel.leftSide + ' ';
+      output += calculator.viewModel.operator;
+      output += calculator.dataModel.dataArray[calculator.dataModel.dataArray.length - 1].value;
+      output += ' = ' + calculator.viewModel.result;
+    }
+
+
+    calculator.$input[0].innerHTML = output;
+
   }
 };
-calculator.init();
+$(document).ready(calculator.init());
